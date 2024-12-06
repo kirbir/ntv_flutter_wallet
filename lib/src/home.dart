4c3e21441754839fe017f58edddcc488b124094f
Birkir Reynisson <109977046+kirbir@users.noreply.github.com>
2024-12-06 22:16:16 +0000

    changes: - import wallet - home screen

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -5,25 +5,29 @@ import '/src/settings/settings_view.dart';
 import 'package:solana/solana.dart';
 import 'package:bs58/bs58.dart' as bs58;
 
-
 class Home extends StatelessWidget {
-   Home({super.key});
+  Home({super.key});
 
   double balance = 0.05;
   final RpcClient client = RpcClient('https://api.devnet.solana.com');
 
- Future<Ed25519HDKeyPair> importPrivateKey(String base58PrivateKey) async {
+  Future<Ed25519HDKeyPair> importPrivateKey(String base58PrivateKey) async {
     final bytes = base58decode(base58PrivateKey);
-    
-    
-    var wallet = await Ed25519HDKeyPair.fromSeedWithHdPath(seed: bytes, hdPath: 'm/44\'/501\'/0\'/0\'');
+    var wallet = await Ed25519HDKeyPair.fromSeedWithHdPath(
+        seed: bytes, hdPath: 'm/44\'/501\'/0\'/0\'');
     return wallet;
+  }
+
+    Future<double> getBalance(String publicKey) async {
+    final balance = await client.getBalance(publicKey);
+    return balance.value / lamportsPerSol; // Convert lamports to SOL
   }
 
   @override
   Widget build(BuildContext context) {
     // Example private key (Base58 encoded)
-    String myPrivateKey = '4hMRe69JA89KCjunzwGM8DNgUAoGkhbq6qXBgxzEQWRF3qYtFpEQD3Bj8HjM34XgrqZzZH5Tw56AFzH3H55BTiXW';
+    String myPrivateKey =
+        '4hMRe69JA89KCjunzwGM8DNgUAoGkhbq6qXBgxzEQWRF3qYtFpEQD3Bj8HjM34XgrqZzZH5Tw56AFzH3H55BTiXW';
     importPrivateKey(myPrivateKey).then((wallet) {
       // Use the wallet here
       print('Imported wallet address: ${wallet.address}');
@@ -46,14 +50,21 @@ class Home extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
-              child: Center(
-                child: Text(
-                  balance.toString(),
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
+            child: Center(
+              child: Text(
+                balance.toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
               ),
+            ),
           ),
-          OutlinedButton(onPressed: () {} , child: Text('Import Wallet'))
+          OutlinedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) => Text('Import Wallet'),
+                );
+              },
+              child: Text('Import Wallet'))
         ],
       ),
     );
